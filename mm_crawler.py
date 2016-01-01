a7df0d812df799b72328465b9dd5ebf107fd1de7
yaowei <yaowei@unitedstack.com>
2016-01-02 02:16:05 +0800

    eventlet

--- mm_crawler.py
+++ mm_crawler.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import urllib2
 import re
 import os
 import sys
-import threadpool
+import Queue
+import eventlet
+import multiprocessing
+from eventlet.green import urllib2
 
-import pdb
+url_queue = Queue.Queue(maxsize=50)
+img_queue = Queue.Queue(maxsize=100)
+path = 'pics'
 
 def help():
     print '-o 选择图片下载目录'
@@ -15,34 +19,56 @@ def help():
     print '-h 显示帮助'
     sys.exit(0)
 
-# 回调函数，下载图片
-def download(url):
-    global path
-    url = url + '.jpg'
-    name = url.replace('/', '_')
-    url = 'http://' + url
-    print url
-    try:
-        jpg = urllib2.urlopen(url).read()
-        f = file(path + '/' + name, 'w')
-        f.write(jpg)
-        f.close()
-    except Exception, e:
-        print e
+
+class HrefProcess(multiprocessing.Process):
+    def __init__(self):
+        super(HrefProcess, self).__init__()
+        self.pool = eventlet.GreenPool()
+
+    def run(self):
+        print 'href process...'
+        while True:
+            url = url_queue.get(block=True)
+            self.pool.spawn_n(self._deep_search, url)
+            self.pool.waitall()
+
+    def _deep_search(self, url):
+        try:
+            print 'deep searching: %s' % url
+            html = urllib2.urlopen(url).read()
+            hrefpattern = re.compile(r'<a\shref="/mm/(.*?)"')
+            hrefs = hrefpattern.findall(html)
+            [url_queue.put('http://22mm.xiuna.com/mm/%s' % url) for href in hrefs]
+            imgpattern = re.compile(r'src\s"http://22mm-img.xiuna.com/pic/(.*?).jpg"')
+            imgs = imgpattern.findall(html)
+            [img_queue.put('http://22mm-img.xiuna.com/pic/%s.jpg' % img) for img in imgs]
+        except Exception, e:
+            print e
+
+class ImgProcess(multiprocessing.Process):
+    def __init__(self):
+        super(ImgProcess, self).__init__()
+        self.pool = eventlet.GreenPool()
+
+    def run(self):
+        print 'img process...'
+        while True:
+            img_url = img_queue.get(block=True)
+            self.pool.spawn_n(self._get_img, img_url)
+            self.pool.waitall()
+
+    def _get_img(self, url):
+        try:
+            name = url[30:]
+            name = name.replace('/', '_')
+            print 'get img: %s' % url
+            jpg = urllib2.urlopen(url).read()
+            f = file(path + '/' + name, 'w')
+            f.write(jpg)
+            f.close()
+        except Exception, e:
+            print e
  
-def deepSearch(url):
-    url = 'http://' + url
-    print url
-    try:
-        html = urllib2.urlopen(url).read()
-        # 从获取到的页面上分别找出图片链接和页面链接
-        hrefpattern = re.compile(r'<a\shref="http://()\""')
-        hreflist = hrefpattern.findall(html)
-        picpattern = re.compile(r'src\s"http://(.*?).jpg"')
-        piclist = picpattern.findall(html)
-        return hreflist, piclist
-    except Exception, e:
-        print e
     
 def main():
     args = sys.argv
@@ -67,25 +93,16 @@ def main():
     if os.path.exists(path):
         os.system('rm -rf {path}'.format(path=path))
     os.mkdir(path)
-    index = 'www.22mm.cc/'
-    hreftp = threadpool.ThreadPool(num_workers=4, resq_size=20)
-    indexreqs = threadpool.makeRequests(deepSearch, [index])
-    [hreftp.putRequest(req) for req in indexreqs]
-    pictp = threadpool.ThreadPool(n)
-    while True:
-        try:
-            hreflist, piclist = hreftp._results_queue.get(block=True)
-            pdb.set_trace()
-            hrefreqs = threadpool.makeRequests(deepSearch, hreflist)
-            [hreftp.putRequest(req) for req in hrefreqs]
-            picreqs = threadpool.makeRequests(download, piclist)
-            [pictp.putRequest(req) for req in picreqs]
-            hreftp.wait()
-            pictp.wait()
-        except Exception, e:
-            print e
+    index = 'http://www.22mm.cc/'
+    url_queue.put(index)
+    href_p = HrefProcess()
+    href_p.daemon = True
+    href_p.start()
+    img_p = ImgProcess()
+    img_p.daemon = True
+    img_p.start()
+    href_p.join()
+    img_p.join()
 
-path = 'pics'
 if __name__ == '__main__':
     main()
-
