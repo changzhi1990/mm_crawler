73d09de9220971f0527b16aeec98186a7f1767cf
yaowei <yaowei@unitedstack.com>
2016-01-03 13:21:20 +0800

    thread pool instaed of eventletOC

--- mm_crawler.py
+++ mm_crawler.py
@@ -6,70 +6,82 @@ import os
 import sys
 import eventlet
 import multiprocessing
+import urllib2
 from multiprocessing import Queue
-from eventlet.green import urllib2
+from multiprocessing.dummy import Pool as ThreadPool
+
 
 url_queue = Queue(maxsize=200)
-url_set = set() # hash list
+url_set = set()  # hash list
 img_queue = Queue(maxsize=400)
-img_set = set() # hash list
+img_set = set()  # hash list
+HREF_THREAD_NUM = 10
+IMG_THREAD_NUM = 20
 path = 'pics'
 
 
 class HrefProcess(multiprocessing.Process):
     def __init__(self):
         super(HrefProcess, self).__init__()
-        self.pool = eventlet.GreenPool()
 
     def run(self):
-        while True:
-            url = url_queue.get(block=True)
-            self.pool.spawn_n(self._breadth_search, url)
-            self.pool.waitall()
+        self.pool = ThreadPool(HREF_THREAD_NUM)
+        self.pool.map(self._breadth_search, range(HREF_THREAD_NUM))
+        self.pool.join()
 
-    def _breadth_search(self, url):
-        try:
-            html = urllib2.urlopen(url).read()
-            hrefpattern = re.compile(r'<a\shref="/mm/(.*?)"')
-            hrefs = hrefpattern.findall(html)
-            for href in hrefs:
-                if href in url_set:
-                    continue
-                url_set.add(href)
-                url_queue.put('http://22mm.xiuna.com/mm/%s' % href)
-            imgpattern = re.compile(r'src="http://22mm-img.xiuna.com/pic/(.*?).jpg"')
-            imgs = imgpattern.findall(html)
-            for img in imgs:
-                if img in img_set:
-                    continue
-                img_set.add(img)
-                img_queue.put('http://22mm-img.xiuna.com/pic/%s.jpg' % img)
-        except Exception, e:
-            print e
+    def _breadth_search(self, thread_id):
+        while True:
+            try:
+                url = url_queue.get(block=True)
+                html = urllib2.urlopen(url).read()
+                hrefpattern = re.compile(r'<a\shref="/mm/(.*?)"')
+                hrefs = hrefpattern.findall(html)
+                for href in hrefs:
+                    if href in url_set:
+                        continue
+                    url_set.add(href)
+                    url_queue.put('http://22mm.xiuna.com/mm/%s' % href)
+                r_str = r'src="http://22mm-img.xiuna.com/pic/(.*?).jpg"'
+                imgpattern = re.compile(r_str)
+                imgs = imgpattern.findall(html)
+                for img in imgs:
+                    if img in img_set:
+                        continue
+                    img_set.add(img)
+                    img_queue.put('http://22mm-img.xiuna.com/pic/%s.jpg' % img)
+            except urllib2.HTTPError as he:
+                print '_bread_search %s' % url
+                print he
+            except Exception as e:
+                print e
+
 
 class ImgProcess(multiprocessing.Process):
     def __init__(self):
         super(ImgProcess, self).__init__()
-        self.pool = eventlet.GreenPool()
 
     def run(self):
-        while True:
-            img_url = img_queue.get(block=True)
-            self.pool.spawn_n(self._get_img, img_url)
-            self.pool.waitall()
+        self.pool = ThreadPool(IMG_THREAD_NUM)
+        self.pool.map(self._get_img, range(IMG_THREAD_NUM))
+        self.pool.join()
 
-    def _get_img(self, url):
-        try:
-            name = url[30:]
-            name = name.replace('/', '_')
-            jpg = urllib2.urlopen(url).read()
-            f = file(path + '/' + name, 'w')
-            f.write(jpg)
-            f.close()
-        except Exception, e:
-            print e
- 
-    
+    def _get_img(self, thread_id):
+        while True:
+            try:
+                url = img_queue.get(block=True)
+                name = url[30:]
+                name = name.replace('/', '_')
+                jpg = urllib2.urlopen(url).read()
+                f = file(path + '/' + name, 'w')
+                f.write(jpg)
+                f.close()
+            except urllib2.HTTPError as he:
+                print '_get_img %s' % url
+                print he
+            except Exception as e:
+                print e
+
+
 def main():
     args = sys.argv
     global path
